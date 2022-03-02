620e3b8d8f7e5368e9f39763bdd1ff81dedd5879
Firuz9014 <72589860+Firuz9014@users.noreply.github.com>
2022-03-01 22:41:29 -0500

    Initial Commit

---
+++ Hotel Booking/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hotel Booking
+//
+//  Created by Фируз Рахмонкулов  on 3/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
